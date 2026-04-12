5fe64f956ba2deb30d84eded12e11c7a593410fe
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2026-04-12 21:36:41 +0900

    theme

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+const colorMode = useColorMode()
+colorMode.preference = 'dark'
+
 useHead({
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
   ],
   htmlAttrs: {
     lang: 'ja'
