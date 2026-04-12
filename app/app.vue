896230dbcc126f55258cc04c739b7ce96a29c066
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2026-04-12 22:04:50 +0900

    OGP移動

--- app/app.vue
+++ app/app.vue
@@ -3,14 +3,6 @@ const colorMode = useColorMode()
 colorMode.preference = 'dark'
 
 useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
-    { property: 'og:title', content: 'FreqThresh' },
-    { property: 'og:description', content: '聴力周波数閾値テスト' },
-    { property: 'og:image', content: '/ogp.svg' },
-    { property: 'og:type', content: 'website' },
-    { name: 'twitter:card', content: 'summary_large_image' }
-  ],
   link: [
     { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
   ],
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,13 @@ export default defineNuxtConfig({
     head: {
       title: 'FreqThresh',
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' }
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, viewport-fit=cover' },
+        { property: 'og:title', content: 'FreqThresh' },
+        { property: 'og:description', content: '聴力周波数閾値テスト' },
+        { property: 'og:image', content: 'https://freq-thresh.vercel.app/ogp.svg' },
+        { property: 'og:url', content: 'https://freq-thresh.vercel.app/' },
+        { property: 'og:type', content: 'website' },
+        { name: 'twitter:card', content: 'summary_large_image' }
       ]
     }
   },
